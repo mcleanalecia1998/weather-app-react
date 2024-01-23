21e57bbc0681837af324ec3e1135c62d7e885843
mcleanalecia1998 <139597906+mcleanalecia1998@users.noreply.github.com>
2024-01-22 20:53:05 -0500

    Style background

--- my-app/src/WeatherIcon.js
+++ my-app/src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="goldenrod"
+      color="lightblue"
       size={512}
       animate={true}
     />
