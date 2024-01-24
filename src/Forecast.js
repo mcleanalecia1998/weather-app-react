3974993d65cdfcd9717a78c7399ac8f9d2e98aac
mcleanalecia1998 <139597906+mcleanalecia1998@users.noreply.github.com>
2024-01-24 16:14:49 -0500

    Update to folder

---
+++ src/Forecast.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+
+import "./Forecast.css";
+import axios from "axios";
+import ForecastDay from "./ForecastDay";
+
+export default function Forecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinate]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let apiKey = `b9ba0314a93083136d968577c718e31d`;
+    let latitude = props.coordinate.lon;
+    let longitude = props.coordinate.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <ForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
+}
